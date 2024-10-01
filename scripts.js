857ac43a8e9d0f4dea94d1fe72b88a133b7e3814
Danilo-sn <danilo_sn@icloud.com>
2024-10-01 17:40:48 -0300

    Meu primeiro commit

---
+++ scripts.js
@@ -0,0 +1,68 @@
+const btnConverter = document.getElementsByClassName("btn_converter")[0];
+const inputReal = document.getElementsByClassName('input_real');
+const valueCourrent = document.getElementsByClassName('value_current');
+const valueName = document.getElementsByClassName('value_name')[1];
+const selectCurrency = document.getElementById('select_current');
+const dolar = 5.50;
+const euro = 5.90;
+const btc = 340940
+
+const corvertValues = () => {
+    const reais = inputReal[0].value;
+
+
+    if (selectCurrency.value === '€ Euro') {
+        valueCourrent[1].innerHTML = new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(
+            reais / euro,
+        );
+
+    }
+
+    if (selectCurrency.value === 'US$ Dólar americano') {
+        valueCourrent[1].innerHTML = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(
+            reais / dolar,
+        );
+
+    }
+
+    if (selectCurrency.value === 'Bitcoin') {
+        valueCourrent[1].innerHTML = new Intl.NumberFormat("en-US", { style: "currency", currency: "BTC", maximumFractionDigits: 8 }).format(
+            reais / btc,
+        );
+
+
+    }
+
+    valueCourrent[0].innerHTML = new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(
+        reais,
+    );
+
+
+};
+
+const changeCurrency = () => {
+    const currencyImg = document.getElementById('currency_img');
+    if (selectCurrency.value === '€ Euro') {
+        valueName.innerHTML = 'Euro';
+        currencyImg.src = './imgs/euro.svg';
+
+    }
+
+    if (selectCurrency.value === 'US$ Dólar americano') {
+        valueName.innerHTML = 'Dólar americano';
+        currencyImg.src = './imgs/estados-unidos.svg'
+
+    }
+
+    if (selectCurrency.value === 'Bitcoin') {
+        valueName.innerHTML = 'Bitcoin';
+        currencyImg.src = './imgs/btc.png'
+
+    }
+
+    corvertValues()
+
+};
+
+btnConverter.addEventListener('click', corvertValues);
+selectCurrency.addEventListener('change', changeCurrency)
